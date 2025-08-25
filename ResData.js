e97367d3d6aade471134a5f5bd0334d1922f723e
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-08-25 12:59:49 +0100

    app ignited, props added

---
+++ ResData.js
@@ -0,0 +1,94 @@
+const resList = [
+  {
+    "id": "1",
+    "name": "KFC",
+    "cuisine": ["burger", "fried chicken", "Hot Wings"],
+    "avgRating": "3.2",
+    "distance": 32,
+    "costForTwo": 400,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "2",
+    "name": "Burger King",
+    "cuisine": ["burger", "fries", "milkshake"],
+    "avgRating": "4.1",
+    "distance": 15,
+    "costForTwo": 350,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "3",
+    "name": "Domino's Pizza",
+    "cuisine": ["pizza", "pasta", "garlic bread"],
+    "avgRating": "3.9",
+    "distance": 45,
+    "costForTwo": 500,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "4",
+    "name": "McDonald's",
+    "cuisine": ["burger", "fries", "soft drinks"],
+    "avgRating": "4.0",
+    "distance": 10,
+    "costForTwo": 300,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "5",
+    "name": "Subway",
+    "cuisine": ["sandwich", "salad", "cookies"],
+    "avgRating": "4.3",
+    "distance": 25,
+    "costForTwo": 250,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "6",
+    "name": "Pizza Hut",
+    "cuisine": ["pizza", "wings", "desserts"],
+    "avgRating": "3.8",
+    "distance": 50,
+    "costForTwo": 550,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "7",
+    "name": "Starbucks",
+    "cuisine": ["coffee", "bakery", "sandwiches"],
+    "avgRating": "4.5",
+    "distance": 5,
+    "costForTwo": 600,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "8",
+    "name": "Taco Bell",
+    "cuisine": ["tacos", "burritos", "nachos"],
+    "avgRating": "3.7",
+    "distance": 20,
+    "costForTwo": 350,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "9",
+    "name": "Dunkin' Donuts",
+    "cuisine": ["donuts", "coffee", "muffins"],
+    "avgRating": "4.2",
+    "distance": 30,
+    "costForTwo": 200,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  },
+  {
+    "id": "10",
+    "name": "Wendy's",
+    "cuisine": ["burger", "frosty", "chicken nuggets"],
+    "avgRating": "4.0",
+    "distance": 18,
+    "costForTwo": 400,
+    "image": "https://uppercrustindia.com/v3/media/postassets/2025/Apr/24/image-20250424171417-4_0.jpeg"
+  }
+];
+
+export default resList;
